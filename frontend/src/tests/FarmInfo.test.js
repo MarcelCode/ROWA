6f05c552d6414562668a5aa5f795ac917b1ce1f7
Emil9999 <emil.sallem@code.berlin>
2020-03-27 12:08:08 +0100

    Added additional infrastructure for tests

--- frontend/src/tests/FarmInfo.test.js
+++ frontend/src/tests/FarmInfo.test.js
@@ -1,15 +1,23 @@
 // Import `shallowMount` from Vue Test Utils and the component being tested
-import { shallowMount } from '@vue/test-utils'
+import { createLocalVue, mount,shallowMount } from '@vue/test-utils'
 import FarmInfo from '../components/home/FarmInfo.vue'
+import vuetify from "vuetify"
+
 
 // Mount the component
-const wrapper = shallowMount(FarmInfo)
-
+//const wrapper = shallowMount(FarmInfo)
 // Here are some Jest tests, though you can
 // use any test runner/assertion library combo you prefer
 describe('FarmInfo', () => {
+  let wrapper;
+  beforeEach(() => {
+    const localVue = createLocalVue()
+    localVue.use(vuetify)
+    wrapper = shallowMount(FarmInfo, {localVue})
+  })
+  
   // Inspect the raw component options
   it('has a created hook', () => {
-    expect(typeof FarmInfo.created).toBe('function')
+    expect(shallowMount(FarmInfo).isVueInstance()).toBe(true)
   })
 })
