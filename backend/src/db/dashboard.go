bddd43314b897e875a1548ad02f12eea8ec4a872
Emil9999 <emil.sallem@code.berlin>
2021-01-17 18:02:07 +0100

    func extraction on getplantspertype

--- backend/src/db/dashboard.go
+++ backend/src/db/dashboard.go
@@ -31,173 +31,189 @@ type PlantInfoPerModule struct {
 	Harvestable   bool   `json:"harvestable"`
 }
 
+
 func (store *Database) GetPlantsPerType(farmAction string) (plantsToHarvest []*PlantsPerPlantType, err error) {
-	sqlQuery := ``
 	switch farmAction {
 	case "harvestable":
-		sqlQuery = `SELECT PlantType, COUNT(PlantType) as AvailablePlantsPerPlantType
+		getHarvestablePlants()
+	case "plantable":
+		getPlantablePlants()
+	case "modules":
+		getAllPlantsInModules()
+	default:
+
+		
+	}
+
+
+	
+	return
+}
+func (store *Database) getHarvestablePlants() (plantsToHarvest []*PlantsPerPlantType, err error){
+	sqlQuery := ``
+	sqlQuery = `SELECT PlantType, COUNT(PlantType) as AvailablePlantsPerPlantType
+				FROM Plant
+						 INNER JOIN Module M on Plant.Module = M.Id
+						 INNER JOIN PlantType PT on M.PlantType = PT.Name
+				where Harvested = 0
+				  and date(PlantDate, '+' || GrowthTime || ' days') <= date('now')
+				GROUP BY PlantType`
+				rows, err := store.Db.Query(sqlQuery)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		plantsPerPlantType := &PlantsPerPlantType{}
+		err = rows.Scan(&plantsPerPlantType.Name, &plantsPerPlantType.AvailablePlants)
+		if err != nil {
+			log.Fatal(err)
+		}
+		plantsToHarvest = append(plantsToHarvest, plantsPerPlantType)
+	}
+	return
+}
+
+
+func (store *Database) getPlantablePlants() (plantsToHarvest []*PlantsPerPlantType, err error){
+	sqlQuery := ``
+
+	for i := 1; i < 7; i++ {
+		sqlQuery = `SELECT COUNT(Id) FROM Plant WHERE Harvested = 0 AND Module = ?`
+		rows, err := store.Db.Query(sqlQuery, i)
+		rows.Next()
+		var id int
+		rows.Scan(&id)
+		
+		if err != nil {
+			log.Fatal(err)
+		}
+		rows.Close()
+		
+		sqlQuery = `SELECT PlantType, COUNT(PlantType) as AvailablePlants
 					FROM Plant
-							 INNER JOIN Module M on Plant.Module = M.Id
-							 INNER JOIN PlantType PT on M.PlantType = PT.Name
-					where Harvested = 0
-					  and date(PlantDate, '+' || GrowthTime || ' days') <= date('now')
-					GROUP BY PlantType`
-					rows, err := store.Db.Query(sqlQuery)
+							INNER JOIN Module M on Plant.Module = M.Id
+							INNER JOIN PlantType PT on M.PlantType = PT.Name
+					WHERE Harvested = 0
+					AND M.Id = ?
+					AND date(PlantDate, '+' || 7 || ' days') <= date('now')`
+		
+		rows, err = store.Db.Query(sqlQuery, i)
 		if err != nil {
 			log.Fatal(err)
 		}
 		defer rows.Close()
-
-		for rows.Next() {
-			plantsPerPlantType := &PlantsPerPlantType{}
-			err = rows.Scan(&plantsPerPlantType.Name, &plantsPerPlantType.AvailablePlants)
-			if err != nil {
-				log.Fatal(err)
-			}
-			plantsToHarvest = append(plantsToHarvest, plantsPerPlantType)
-		}
-	case "plantable":
-		for i := 1; i < 7; i++ {
-			sqlQuery = `SELECT COUNT(Id) FROM Plant WHERE Harvested = 0 AND Module = ?`
-			rows, err := store.Db.Query(sqlQuery, i)
-			rows.Next()
-			var id int
-			rows.Scan(&id)
-			
-			if err != nil {
-				log.Fatal(err)
-			}
-			rows.Close()
-			
-			sqlQuery = `SELECT PlantType, COUNT(PlantType) as AvailablePlants
-						FROM Plant
-								INNER JOIN Module M on Plant.Module = M.Id
-								INNER JOIN PlantType PT on M.PlantType = PT.Name
-						WHERE Harvested = 0
-						AND M.Id = ?
-						AND date(PlantDate, '+' || 7 || ' days') <= date('now')`
-			
-			rows, err = store.Db.Query(sqlQuery, i)
-			if err != nil {
-				log.Fatal(err)
-			}
-			defer rows.Close()
-			
-			for rows.Next() {
-					plantsPerPlantType := &PlantsPerPlantType{}
-					err = rows.Scan(&plantsPerPlantType.Name, &plantsPerPlantType.AvailablePlants)
-					if err != nil {
-						sqlQuery = `SELECT PlantType
-								FROM Module 
-								WHERE Id = ?`
-								rows, err = store.Db.Query(sqlQuery, i)
-								var plantType string
-								rows.Next()
-								rows.Scan(&plantType)
-								if (err != nil) {log.Print(err)}
-								plantsPerPlantType := &PlantsPerPlantType{}
-								plantsPerPlantType.Name = plantType
-								plantsPerPlantType.AvailablePlants = 6
-								if plantsPerPlantType.AvailablePlants-id == 0 && 6-id > 0 {
-									p, found := find(plantsToHarvest, plantsPerPlantType.Name)
-									if found {
-										plantsToHarvest[p].AvailablePlants++
-										log.Print(plantsPerPlantType)
-									} else {
-										plantsPerPlantType.AvailablePlants = 1
-										plantsToHarvest = append(plantsToHarvest, plantsPerPlantType)
-										log.Print(plantsPerPlantType)
-									}
-								}
-								break
-					}
-					if plantsPerPlantType.AvailablePlants-id == 0 && 6-id > 0 {
-						p, found := find(plantsToHarvest, plantsPerPlantType.Name)
-						if found {
-							plantsToHarvest[p].AvailablePlants++
-							log.Print(plantsPerPlantType)
-						} else {
-							plantsPerPlantType.AvailablePlants = 1
-							plantsToHarvest = append(plantsToHarvest, plantsPerPlantType)
-							log.Print(plantsPerPlantType)
-						}
-						
-				 
-					
-								
-				 }
-				
-				
-				
-				
-	
-		}}
-	case "modules":
-		for i := 1; i < 7; i++ {
-			sqlQuery = `SELECT COUNT(Id) FROM Plant WHERE Harvested = 0 AND Module = ?`
-			rows, err := store.Db.Query(sqlQuery, i)
-			rows.Next()
-			var id int
-			rows.Scan(&id)
-			
-			if err != nil {
-				log.Fatal(err)
-			}
-			rows.Close()
-			
-			sqlQuery = `SELECT PlantType, COUNT(PlantType) as AvailablePlants
-						FROM Plant
-								INNER JOIN Module M on Plant.Module = M.Id
-								INNER JOIN PlantType PT on M.PlantType = PT.Name
-						WHERE Harvested = 0
-						AND M.Id = ?
-						AND date(PlantDate, '+' || 7 || ' days') <= date('now')`
-			
-			rows, err = store.Db.Query(sqlQuery, i)
-			if err != nil {
-				log.Fatal(err)
-			}
-			
-			defer rows.Close()
 		
-			for rows.Next() {
-				
-				
+		for rows.Next() {
 				plantsPerPlantType := &PlantsPerPlantType{}
 				err = rows.Scan(&plantsPerPlantType.Name, &plantsPerPlantType.AvailablePlants)
+				if err != nil {
+					sqlQuery = `SELECT PlantType
+							FROM Module 
+							WHERE Id = ?`
+							rows, err = store.Db.Query(sqlQuery, i)
+							var plantType string
+							rows.Next()
+							rows.Scan(&plantType)
+							if (err != nil) {log.Print(err)}
+							plantsPerPlantType := &PlantsPerPlantType{}
+							plantsPerPlantType.Name = plantType
+							plantsPerPlantType.AvailablePlants = 6
+							if plantsPerPlantType.AvailablePlants-id == 0 && 6-id > 0 {
+								p, found := find(plantsToHarvest, plantsPerPlantType.Name)
+								if found {
+									plantsToHarvest[p].AvailablePlants++
+									log.Print(plantsPerPlantType)
+								} else {
+									plantsPerPlantType.AvailablePlants = 1
+									plantsToHarvest = append(plantsToHarvest, plantsPerPlantType)
+									log.Print(plantsPerPlantType)
+								}
+							}
+							break
+				}
+				if plantsPerPlantType.AvailablePlants-id == 0 && 6-id > 0 {
+					p, found := find(plantsToHarvest, plantsPerPlantType.Name)
+					if found {
+						plantsToHarvest[p].AvailablePlants++
+						log.Print(plantsPerPlantType)
+					} else {
+						plantsPerPlantType.AvailablePlants = 1
+						plantsToHarvest = append(plantsToHarvest, plantsPerPlantType)
+						log.Print(plantsPerPlantType)
+					}
+							
+			 }
+		
+
+	}
+}
+return
+}
+
+func (store *Database) getAllPlantsInModules() (plantsToHarvest []*PlantsPerPlantType, err error) {
+	sqlQuery := ``
+	for i := 1; i < 7; i++ {
+		sqlQuery = `SELECT COUNT(Id) FROM Plant WHERE Harvested = 0 AND Module = ?`
+		rows, err := store.Db.Query(sqlQuery, i)
+		rows.Next()
+		var id int
+		rows.Scan(&id)
+		
+		if err != nil {
+			log.Fatal(err)
+		}
+		rows.Close()
+		
+		sqlQuery = `SELECT PlantType, COUNT(PlantType) as AvailablePlants
+					FROM Plant
+							INNER JOIN Module M on Plant.Module = M.Id
+							INNER JOIN PlantType PT on M.PlantType = PT.Name
+					WHERE Harvested = 0
+					AND M.Id = ?
+					AND date(PlantDate, '+' || 7 || ' days') <= date('now')`
+		
+		rows, err = store.Db.Query(sqlQuery, i)
+		if err != nil {
+			log.Fatal(err)
+		}
+		
+		defer rows.Close()
+	
+		for rows.Next() {
+			
+			
+			plantsPerPlantType := &PlantsPerPlantType{}
+			err = rows.Scan(&plantsPerPlantType.Name, &plantsPerPlantType.AvailablePlants)
+			if plantsPerPlantType.AvailablePlants-id == 0 && 6-id > 0 {
+			plantsPerPlantType.AvailablePlants = i
+			if err != nil {
+				sqlQuery = `SELECT PlantType
+				FROM Module 
+				WHERE Id = ?`
+				rows, err = store.Db.Query(sqlQuery, i)
+				var plantType string
+				rows.Scan(&plantType)
+				plantsPerPlantType := &PlantsPerPlantType{}
+				plantsPerPlantType.Name = plantType
+				plantsPerPlantType.AvailablePlants = 6
 				if plantsPerPlantType.AvailablePlants-id == 0 && 6-id > 0 {
 				plantsPerPlantType.AvailablePlants = i
 				if err != nil {
-					sqlQuery = `SELECT PlantType
-					FROM Module 
-					WHERE Id = ?`
-					rows, err = store.Db.Query(sqlQuery, i)
-					var plantType string
-					rows.Scan(&plantType)
-					plantsPerPlantType := &PlantsPerPlantType{}
-					plantsPerPlantType.Name = plantType
-					plantsPerPlantType.AvailablePlants = 6
-					if plantsPerPlantType.AvailablePlants-id == 0 && 6-id > 0 {
-					plantsPerPlantType.AvailablePlants = i
-					if err != nil {
-						log.Print(err)
-					}
-					plantsToHarvest = append(plantsToHarvest, plantsPerPlantType)}
-					break
+					log.Print(err)
 				}
 				plantsToHarvest = append(plantsToHarvest, plantsPerPlantType)}
 				break
-			
-			} }
-		/*sqlQuery = `SELECT PlantType, SUM(AvailableSpots) as AvailablePlants
-		FROM Module
-		where AvailableSpots > 0
-		GROUP BY PlantType`*/
-	default:
-		log.Fatal("Wrong parameter passed into function")
-	}
-	return
+			}
+			plantsToHarvest = append(plantsToHarvest, plantsPerPlantType)}
+			break
+		
+		} }
+		return
+
 }
+
 
 func (store *Database) GetLastSensorEntry() (sensorData *SensorData, err error) {
 	sqlQuery := `SELECT Datetime, Temp, LightIntensity, Humidity, WaterLevel, WaterTemp, WaterpH
